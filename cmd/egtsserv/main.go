6a3271c9189cd01e7e9218b908123c1a30b71992
egorban <gorban@satissoft.ru>
2019-09-17 17:47:24 +0300

    fix

--- cmd/egtsserv/main.go
+++ cmd/egtsserv/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"github.com/gorban/egtsServ/pkg/egtsserv"
+	"github.com/egorban/egtsServ/pkg/egtsserv"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/egorban/egtsServ
 
 go 1.12
+
+require github.com/ashirko/navprot v1.4.0
