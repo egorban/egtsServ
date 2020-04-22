d4a217f136f022fa8649e9ed9c32229a9846922a
egorban <gorban@satissoft.ru>
2020-04-23 01:23:56 +0300

    fix

--- cmd/egtsserv/main.go
+++ cmd/egtsserv/main.go
@@ -7,9 +7,7 @@ import (
 
 func main() {
 	var listenPort string
-	var numPackets int
 	flag.StringVar(&listenPort, "p", "9002", "listen port (e.g. 'localhost:9002')")
-	flag.IntVar(&numPackets, "n", 100, "num received packets (e.g. 100)")
 	flag.Parse()
-	egtsserv.Start(listenPort, numPackets)
+	egtsserv.Start(listenPort)
 }
--- pkg/egtsserv/egtsserv.go
+++ pkg/egtsserv/egtsserv.go
@@ -4,20 +4,7 @@ import (
 	"github.com/ashirko/navprot/pkg/egts"
 	"log"
 	"net"
-	"sync"
 	"time"
-)
-
-type Result struct {
-	numConn    uint64
-	numReceive int
-}
-
-var (
-	running     bool
-	muRun       sync.Mutex
-	newConnChan chan uint64
-	closeConn   chan Result
 )
 
 const (
@@ -26,79 +13,35 @@ const (
 	readTimeout       = 180 * time.Second
 )
 
-func Start(listenPort string, numPackets int) {
+func Start(listenPort string) {
 	listenAddress := "localhost:" + listenPort
 	l, err := net.Listen("tcp", listenAddress)
 	if err != nil {
 		log.Printf("error while listening: %s", err)
 		return
 	}
-	running = true
 	defer l.Close()
-	log.Printf("EGTS server was started. Listen address: %v; Number packets to receive: %v",
-		listenAddress, numPackets)
-	newConnChan = make(chan uint64)
-	closeConn = make(chan Result)
-	go func() {
-		connNo := uint64(1)
-		for {
-			log.Printf("wait accept...")
-			c, err := l.Accept()
-			if err != nil {
-				muRun.Lock()
-				if !running {
-					muRun.Unlock()
-					return
-				}
-				muRun.Unlock()
-				log.Printf("error while accepting: %s", err)
-				return
-			}
-			defer c.Close()
-			log.Printf("accepted connection %d (%s <-> %s)", connNo, c.RemoteAddr(), c.LocalAddr())
-			go handleConnection(c, connNo, numPackets)
-			newConnChan <- connNo
-			connNo++
-		}
-	}()
-	results := waitStop()
-	muRun.Lock()
-	running = false
-	muRun.Unlock()
-	log.Printf("EGTS server has completed work")
-	for _, r := range results {
-		endingReceive := ""
-		if r.numReceive > 1 {
-			endingReceive = "s"
-		}
-		log.Printf("For connection %d: received %d data packet"+endingReceive,
-			r.numConn, r.numReceive)
-	}
-}
-
-func waitStop() []Result {
-	numsConn := uint64(0)
-	results := make([]Result, 0)
+	log.Printf("EGTS server was started. Listen address: %v", listenAddress)
+	connNo := uint64(1)
 	for {
-		select {
-		case <-newConnChan:
-			numsConn++
-		case res := <-closeConn:
-			results = append(results, res)
-			numsConn--
-			if numsConn == 0 {
-				return results
-			}
+		log.Printf("wait accept...")
+		c, err := l.Accept()
+		if err != nil {
+			log.Printf("error while accepting: %s", err)
+			return
 		}
+		defer c.Close()
+		log.Printf("accepted connection %d (%s <-> %s)", connNo, c.RemoteAddr(), c.LocalAddr())
+		go handleConnection(c, connNo)
+		connNo++
 	}
 }
 
-func handleConnection(conn net.Conn, connNo uint64, numPacketsToReceive int) {
-	res := Result{connNo, 0}
+func handleConnection(conn net.Conn, connNo uint64) {
 	var ansPID uint16
 	var ansRID uint16
 	var restBuf []byte
-	for res.numReceive < numPacketsToReceive {
+	for {
 		err := conn.SetReadDeadline(time.Now().Add(readTimeout))
 		if err != nil {
 			log.Printf("can't set read deadline %s", err)
@@ -118,7 +61,6 @@ func handleConnection(conn net.Conn, connNo uint64, numPacketsToReceive int) {
 				break
 			}
 			log.Printf("receive egts packet: %s", egtsPack.String())
-			res.numReceive++
 			responsePack, err := formResponse(egtsPack, ansPID, ansRID)
 			if err != nil {
 				log.Printf(" error while form response: %v", err)
@@ -139,12 +81,6 @@ func handleConnection(conn net.Conn, connNo uint64, numPacketsToReceive int) {
 			log.Printf("send reply: %s", egtsPack.String())
 		}
 	}
-	time.Sleep(5 * time.Second)
-	err := conn.Close()
-	if err != nil {
-		log.Printf(" error while close connection %d: %v", connNo, err)
-	}
-	closeConn <- res
 }
 
 func formResponse(egtsPack *egts.Packet, ansPID uint16, ansRID uint16) (responsePack []byte, err error) {
