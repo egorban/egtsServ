f7a49021a6bc137d43d4157cea2de8c775ed6d46
gorban <>
2021-02-10 21:36:04 +0300

    fix

--- cmd/egtsserv/main.go
+++ cmd/egtsserv/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+
 	"github.com/egorban/egtsServ/pkg/egtsserv"
 )
 
 func main() {
-	var listenPort string
-	flag.StringVar(&listenPort, "p", "9002", "listen port (e.g. 'localhost:9002')")
+	var listenAddress string
+	flag.StringVar(&listenAddress, "l", "localhost:9002", "listen address (e.g. 'localhost:9002')")
 	flag.Parse()
-	egtsserv.Start(listenPort)
+	egtsserv.Start(listenAddress)
 }
